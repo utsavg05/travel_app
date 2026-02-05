b6459fd12c1d177e2f98aeb88859faa6627c993f
utsavg05 <utsav.gp1204@gmail.com>
2026-02-06 00:35:58 +0530

    fix: replace Slot component with AppNavigator and update layout structure in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
-import { Slot } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { colors } from '../src/constants/theme';
+import AppNavigator from '../src/Navigation/AppNavigator';
+import { StatusBar } from 'react-native';
 
 export default function RootLayout() {
   return (
     <>
-      <Slot />
+    <StatusBar barStyle="dark-content" backgroundColor={colors.background} />
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
+      <AppNavigator />
+    </SafeAreaView>
     </>
   );
 }

e8496b3eb7369ec54390cea57bf7b5412247d11d
utsavg05 <utsav.gp1204@gmail.com>
2026-02-06 14:11:39 +0530

    fix: refactor Index component and update AppNavigator to use TabNavigator

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,3 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { colors } from '../src/constants/theme';
-import AppNavigator from '../src/Navigation/AppNavigator';
-
 export default function Index() {
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
-      <AppNavigator />
-    </SafeAreaView>
-  );
+  return null;
 }
--- src/Navigation/AppNavigator.tsx
+++ src/Navigation/AppNavigator.tsx
@@ -1,13 +1,48 @@
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// import HomeScreen from '../screens/HomeScreen';
+// import PropertyDetailsScreen from '../screens/PropertyDetailsScreen';
+// import DateSelectionScreen from '../screens/DateSelectionScreen';
+// import TravelPartyScreen from '../screens/TravelPartyScreen';
+// import BookingConfirmedScreen from '../screens/BookingConfirmedScreen';
+// import TripsScreen from '../screens/TripsScreen';
+
+// export type RootStackParamList = {
+//   Home: undefined;
+//   PropertyDetails: undefined;
+//   DateSelection: undefined;
+//   TravelParty: undefined;
+//   BookingConfirmed: undefined;
+//   Trips: undefined;
+// }
+
+// const Stack = createNativeStackNavigator<RootStackParamList>();
+
+// export default function AppNavigator() {
+//   return (
+//     <Stack.Navigator screenOptions={{ headerShown: false }}>
+//       <Stack.Screen name="Home" component={HomeScreen} />
+//       <Stack.Screen name="PropertyDetails" component={PropertyDetailsScreen} />
+//       <Stack.Screen name="DateSelection" component={DateSelectionScreen} />
+//       <Stack.Screen name="TravelParty" component={TravelPartyScreen} />
+//       <Stack.Screen name="BookingConfirmed" component={BookingConfirmedScreen} />
+//       <Stack.Screen name="Trips" component={TripsScreen} />
+//     </Stack.Navigator>
+//   );
+// }
+
+
+
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomeScreen from '../screens/HomeScreen';
+import TabNavigator from './TabNavigator';
 import PropertyDetailsScreen from '../screens/PropertyDetailsScreen';
 import DateSelectionScreen from '../screens/DateSelectionScreen';
 import TravelPartyScreen from '../screens/TravelPartyScreen';
 import BookingConfirmedScreen from '../screens/BookingConfirmedScreen';
 
 export type RootStackParamList = {
-  Home: undefined;
+  Tabs: undefined;
   PropertyDetails: undefined;
   DateSelection: undefined;
   TravelParty: undefined;
@@ -19,7 +54,7 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function AppNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Tabs" component={TabNavigator} />
       <Stack.Screen name="PropertyDetails" component={PropertyDetailsScreen} />
       <Stack.Screen name="DateSelection" component={DateSelectionScreen} />
       <Stack.Screen name="TravelParty" component={TravelPartyScreen} />
